6f8ac31d7748f74acf28f76ddecacd28ffc12f7d
Can Tükücü <108419327+Can-Tkcu@users.noreply.github.com>
2022-11-20 18:31:52 +0100

    save

--- js/script.js
+++ js/script.js
@@ -22,6 +22,7 @@ async function loadAllPokemon() {
   console.log(allPokemon);
 }
 
+
 function renderPokemonCards(pokemon) {
   let cardDeck = getDoc("pokemon-container");
   cardDeck.innerHTML += /*html*/ `
@@ -59,63 +60,11 @@ function renderPokemonCards(pokemon) {
       `;
 }
 
-function pokemonColor(type) {
-  if (type == "electric") {
-    return "FFD76F";
-  }
-  if (type == "grass") {
-    return "49D0B0";
-  }
-  if (type == "water") {
-    return "76BEFE";
-  }
-  if (type == "fire") {
-    return "F18788";
-  }
-  if (type == "bug") {
-    return "99d893";
-  }
-  if (type == "normal") {
-    return "d2bd90";
-  }
-  if (type == "poison") {
-    return "c087bf";
-  }
-  if (type == "ground") {
-    return "dd9e66";
-  }
-  if (type == "fairy") {
-    return "f0c2e9";
-  }
-  if (type == "fighting") {
-    return "ffbd62";
-  }
-  if (type == "rock") {
-    return "9d9e92";
-  }
-  if (type == "psychic") {
-    return "ff76e4";
-  }
-  if (type == "ghost") {
-    return "af5feb";
-  }
-  if (type == "ice") {
-    return "b8deff";
-  }
-  if (type == "dragon") {
-    return "2b9cfd";
-  }
-  if (type == "dark") {
-    return "2b9cfd";
-  }
-  if (type == "steel") {
-    return "b1beca";
-  }
-}
 
 function stopProp(event) {
   event.stopPropagation();
 }
+
 
 window.onscroll = async function () {
   if (window.scrollY + window.innerHeight >= document.body.clientHeight - 2) {
