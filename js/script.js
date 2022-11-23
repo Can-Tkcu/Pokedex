d659c18d325ddbcf30438d1e765c3e52f16824a8
Can Tükücü <108419327+Can-Tkcu@users.noreply.github.com>
2022-11-23 16:15:44 +0100

    save

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 let loadedData = [];
 
+let loadedSpecies = [];
+
+let loadedText = [];
+
 let offset = 0;
 
 const getDoc = function (id) {
@@ -16,13 +20,30 @@ async function loadAllPokemon() {
   let response = await fetch(url);
   let loadedPokemon = await response.json();
   for (let i = 0; i < (await loadedPokemon["results"].length); i++) {
-    let pokeURL = loadedPokemon["results"][i]["url"];
-    let pokeResponse = await fetch(pokeURL);
-    let currentPokemon = await pokeResponse.json();
-    loadedData.push(currentPokemon);
+    await loadData(loadedPokemon["results"][i]);
+    
   }
   renderPokemonCards();
   console.log(loadedData);
+}
+
+async function loadData(loadedPokemon) {
+  let pokeURL = loadedPokemon["url"];
+  let pokeResponse = await fetch(pokeURL);
+  let currentPokemon = await pokeResponse.json();
+  await loadSpecies(currentPokemon);
+  loadedData.push(currentPokemon);
+}
+
+async function loadSpecies(currentPokemon) {
+    let speciesURL = currentPokemon.species.url;
+    let speciesResponse = await fetch(speciesURL);
+    let currentPokemonSpecies = await speciesResponse.json();
+    let genus = currentPokemonSpecies.genera[7].genus;
+    let flavorText = currentPokemonSpecies.flavor_text_entries[3].flavor_text;
+    
+    loadedText.push(flavorText);
+    loadedSpecies.push(genus);
 }
 
 function renderPokemonCards() {
@@ -46,25 +67,32 @@ function stopProp(event) {
 async function openPokemonCard(ID) {
   let popupContainer = getDoc("pokemon-popup-container");
   let pokemon = loadedData[ID - 1];
-
-  let speciesURL = pokemon.species.url;
-  let speciesResponse = await fetch(speciesURL);
-  let currentPokemonSpecies = await speciesResponse.json();
-  let genus = currentPokemonSpecies.genera[7].genus;
+  let genus = loadedSpecies[ID - 1];
+  let flavorText = loadedText[ID - 1];
 
   popupContainer.classList.remove("d-none");
   popupContainer.innerHTML = "";
   if (pokemon.types.length == 2) {
-    popupContainer.innerHTML += generatePokeCardPopupTwoTypes(pokemon, genus);
+
+    popupContainer.innerHTML += generatePokeCardPopupTwoTypes(
+      pokemon,
+      genus,
+      flavorText
+    );
     await includeHTML();
     showCard();
-    // console.clear();
+    selectTab("aboutS");
     console.log(pokemon);
   } else {
-    popupContainer.innerHTML += generatePokeCardPopup(pokemon);
+
+    popupContainer.innerHTML += generatePokeCardPopup(
+      pokemon,
+      genus,
+      flavorText
+    );
     await includeHTML();
     showCard();
-    // console.clear();
+    selectTab("aboutS");
     console.log(pokemon);
   }
 }
@@ -76,15 +104,13 @@ function showCard() {
   }, 125);
 }
 
-function generatePokeCardPopupTwoTypes(pokemon, genus) {
+function generatePokeCardPopupTwoTypes(pokemon, genus, flavorText) {
   return /*html*/ `
     <div onclick="stopProp(event)" id="pokemon_card_popup_wrapper" style="background: #${pokemonColor(
       pokemon.types[0].type.name
     )}">
       <div id="pokemon_popup_card">
-          
           <div id="pokemon_popup_img_wrapper">
-            
             <div id="pokemon_popup_type_wrapper">
             <div id="pokemon_popup_header_exit"><img onclick="closePopup()" src="./img/back-arrow.png"></div>
                 <div id="pokemon_popup_card_header">
@@ -119,40 +145,62 @@ function generatePokeCardPopupTwoTypes(pokemon, genus) {
         pokemon.types[0].type.name
       )}">
       <div w3-include-html="templates/detail_header.html"></div>
+        <div class="fm-electro-400" id="about_content">
+        <span id="flavor_text">${flavorText
+          .replace("\f", "\n")
+          .replace("\u00ad\n", "")
+          .replace("\u00ad", "")
+          .replace(" -\n", " - ")
+          .replace("-\n", "-")
+          .replace("\n", " ")}</span>
+          <span id="pokemon_height">Pokemon height: ${pokemon.height + "0"}cm</span>
+          <span id="pokemon_height">Pokemon weight: ${pokemon.weight / 10}kg</span>
+        </div>
       </div>
     </div>
 `;
 }
 
-function generatePokeCardPopup(pokemon) {
+function generatePokeCardPopup(pokemon, genus) {
   return /*html*/ ` 
   <div onclick="stopProp(event)" id="pokemon_card_popup_wrapper" style="background: #${pokemonColor(
     pokemon.types[0].type.name
   )}">
-    <div id="pokemon_popup_card">
-        <div id="pokemon_popup_card_header">
-            <h1 class="fm-electro-400" id="pokemon_popup_name">${
-              pokemon.name
-            }</h1>
-             <b>
-              <span class="fm-electro-400" id="pokemon_popup_id">#${pokemon.id
-                .toString()
-                .padStart(3, "0")}
-              </span>
-            </b>
-        </div>
-        <div id="pokemon_popup_img_wrapper">
-          <div id="pokemon_popup_type_wrapper">
-            <span class="fm-electro-400" id="pokemon_popup_type_name" style="background: #${pokemonColor(
-              pokemon.types[0].type.name
-            )}">${pokemon.types[0].type.name}</span>
+      <div id="pokemon_popup_card">
+          <div id="pokemon_popup_img_wrapper">
+            <div id="pokemon_popup_type_wrapper">
+            <div id="pokemon_popup_header_exit"><img onclick="closePopup()" src="./img/back-arrow.png"></div>
+                <div id="pokemon_popup_card_header">
+                  <h1 class="fm-electro-400" id="pokemon_popup_name">${
+                    pokemon.name
+                  }</h1>
+                  <b>
+                    <span class="fm-electro-400" id="pokemon_popup_id">#${pokemon.id
+                      .toString()
+                      .padStart(3, "0")}
+                    </span>
+                  </b>
+              </div>
+              <div id="pokemon_popup_types">
+                <div id="type_wrapper">        
+                  <span class="fm-electro-400" id="pokemon_popup_type_name" style="background: #${pokemonColor(
+                    pokemon.types[0].type.name
+                  )}">${pokemon.types[0].type.name}</span>
+                </div>
+                <span class="fm-electro-400" id="pokemon_genus">${genus}</span>
+              </div>
+            </div>
+            <img id="pokemon_popup_img" src="${
+              pokemon.sprites.other.dream_world.front_default
+            }">
           </div>
-          <img id="pokemon_popup_img" src="${
-            pokemon.sprites.other.dream_world.front_default
-          }">
-        </div>
+      </div>
+      <div id="pokemon_popup_detail" style="background: #${pokemonColor(
+        pokemon.types[0].type.name
+      )}">
+      <div w3-include-html="templates/detail_header.html"></div>
+      </div>
     </div>
-  </div>
 `;
 }
 
@@ -160,7 +208,6 @@ async function closePopup() {
   let popupContainer = getDoc("pokemon-popup-container");
   let popupCard = getDoc("pokemon_card_popup_wrapper");
   popupCard.classList.remove("scale");
-
   setTimeout(function () {
     popupContainer.classList.add("d-none");
   }, 225);
@@ -172,3 +219,15 @@ window.onscroll = async function (ev) {
     await loadAllPokemon();
   }
 };
+
+function selectTab(ID) {
+  clearStyles();
+  getDoc(ID).style = "background: white";
+}
+
+function clearStyles() {
+  getDoc("aboutS").style = "";
+  getDoc("base_statsS").style = "";
+  getDoc("evolutionS").style = "";
+  getDoc("movesS").style = "";
+}
